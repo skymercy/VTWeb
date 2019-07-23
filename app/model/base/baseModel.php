fc6cc1c771f4253f41817318aa1db244c775bc64
laozhou <laozhou@skyoho.com>
2019-07-23 18:27:22 +0800

    1) 扩展baseModel基类，支持条件查询

--- app/model/base/baseModel.php
+++ app/model/base/baseModel.php
@@ -17,6 +17,7 @@ use biny\lib\BinyString;
  */
 class baseModel
 {
+	protected static $_keyMaps = [];
     /**
      * @var array 单例对象
      */
@@ -29,18 +30,61 @@ class baseModel
      */
     protected $DAO = null;
     protected $_pk;
-
-    /**
-     * @param null $id
-     * @return person
-     */
-    public static function init($id=null)
-    {
-        if (!isset(static::$_instance[$id])){
-            static::$_instance[$id] = new static($id);
-        }
-        return static::$_instance[$id];
-    }
+	
+	/**
+	 * @param null|mixed $id
+	 * @return baseModel
+	 */
+	public static function init($id=null)
+	{
+		if (is_array($id)) {
+			//如果$id是数组,则参数内容为查询条件
+			ksort($id);
+			$key = md5(json_encode($id));
+			if (isset(static::$_keyMaps[$key])) {
+				return static::$_instance[static::$_keyMaps[$key]];
+			}
+			/**@var baseModel $model */
+			$model = new static($id);
+			
+			$pkVal = $model->getPkVal();
+			if (isset(static::$_instance[$pkVal])) {
+				if ($model->exist()) {
+					self::$_instance[$pkVal]->merge($model->attributes());
+				}
+			} else {
+				static::$_instance[$pkVal] = $model;
+			}
+			if (!isset(static::$_keyMaps[$pkVal])) {
+				static::$_keyMaps[$pkVal] = [];
+			}
+			static::$_keyMaps[$pkVal][] = $key;
+			static::$_keyMaps[$key] = $pkVal;
+			
+			return static::$_instance[$pkVal];
+			
+		} else {
+			if (!isset(static::$_instance[$id])){
+				/**@var baseModel $model */
+				static::$_instance[$id] = new static($id);
+			}
+			return static::$_instance[$id];
+		}
+	}
+	
+	protected function __construct($id)
+	{
+		if ($id !== NULL){
+			if (is_array($id)) {
+				$pkName = $this->DAO->getPk();
+				$this->_data = $this->DAO->filter($id)->order([$pkName => 'desc'])->find();
+				$this->_pk = ($this->_data && isset($this->_data[$pkName])) ? $this->_data[$pkName] : '';
+			} else {
+				$this->_data = $this->DAO->getByPk($id);
+				$this->_pk = $id;
+			}
+		}
+	}
 
     public function __get($key)
     {
@@ -79,6 +123,48 @@ class baseModel
             $this->_dirty = false;
         }
     }
+	
+	/**
+	 * 是否存在
+	 * @return mixed
+	 */
+	public function exist()
+	{
+		return $this->_data ? true : false;
+	}
+	
+	/**
+	 * 获取键值
+	 * @return mixed
+	 */
+	public function getPk()
+	{
+		return $this->_pk;
+	}
+	
+	public function attributes() {
+		return $this->_data;
+	}
+	
+	public function merge($data) {
+		$this->_data = \YiiArray::merge($this->_data, $data);
+	}
+	
+	public function deleteFromDb() {
+		$this->DAO->deleteByPk($this->_pk);
+		$this->DAO->clearCache();
+		unset(self::$_instance[$this->_pk]);
+		if (isset(self::$_keyMaps[$this->_pk])) {
+			foreach (self::$_keyMaps[$this->_pk] as $k) {
+				unset(self::$_keyMaps[$k]);
+			}
+			unset(self::$_keyMaps[$this->_pk]);
+		}
+	}
+	
+	public function getPkVal() {
+		return $this->{$this->DAO->getPk()};
+	}
 
 
     public function __toLogger()
