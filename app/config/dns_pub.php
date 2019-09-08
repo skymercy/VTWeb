4534fafc139fce6dc9fa6495cd6c16e173f86851
laozhou <laozhou@skyoho.com>
2019-09-08 22:21:38 +0800

    1) fix

--- app/config/dns_pub.php
+++ app/config/dns_pub.php
@@ -1,17 +1,15 @@
 <?php
 return array(
-    'database' => array(
-        'host'     => '127.0.0.1',
-        'database' => 'Biny',
-        'user'     => 'root',
-        'password' => 'root',
-        'encode' => 'utf8',
-        'port' => 3306,
-    ),
-    'redis' => array(
-        'host' => 'localhost',
-        'port' => 6379,
-//        'client' => 'predis', // predis
-//        'persistent' => true, // predis
-    ),
+	'database' => array(
+		'host'     => '172.18.0.1',
+		'database' => 'db_vtweb',
+		'user'     => 'root',
+		'password' => 'u79Wlaozhou',
+		'encode' => 'utf8mb4',
+		'port' => 3306,
+	),
+	'redis' => array(
+		'host' => '172.18.0.1',
+		'port' => 6379
+	),
 );
