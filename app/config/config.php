00c925a5c0ff1abb026404351ac9ad9a72d224e5
laozhou <laozhou@skyoho.com>
2020-02-12 16:47:20 +0800

    1) fix

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,14 @@
 <?php
-return array(
-    'webRoot' => '@web@'
+
+$_tmpLocalConfig = [];
+
+if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'config-local.php')) {
+    $_tmpLocalConfig = require_once __DIR__ . DIRECTORY_SEPARATOR . 'config-local.php';
+}
+
+return YiiArray::merge(
+    [
+        'webRoot' => '@web@',
+    ]
+    ,$_tmpLocalConfig
 );
